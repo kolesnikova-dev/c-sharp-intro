3c23ac13adeea38ea3c90b495b0de361991b6520
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-05-20 15:23:39 -0400

    feat(ConsoleApp): Add Pastel as dependency and add color to console output

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using Pastel;
+
+Console.WriteLine("Hello, World!".Pastel(Color.DarkBlue));
 Console.WriteLine("The current time is " + DateTime.Now);
